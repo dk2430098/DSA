e425ad7fdefec690b28370e58698c26ee237055d
dk2430098 <dk2430098@gmail.com>
2025-06-18 01:10:46 +0530

    Optimal Sol

--- two_sum.cpp
+++ two_sum.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 using namespace std;
 
-class Solution
+class Solution2
 {
 public:
     vector<int> twoSum(vector<int> &nums, int target)
@@ -21,9 +22,34 @@ public:
     }
 };
 
+class Solution1
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        unordered_map<int, int> mp;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int complement = target - nums[i];
+
+            // Check if complement exists
+            if (mp.find(complement) != mp.end())
+            {
+                return {mp[complement], i}; // Found the pair
+            }
+
+            // Store current number with its index
+            mp[nums[i]] = i;
+        }
+
+        return {}; // Return empty vector if no pair found
+    }
+};
+
 int main()
 {
-    Solution sol;
+    Solution2 sol;
     vector<int> v = {2, 7, 11, 15};
     int target = 9;
 
