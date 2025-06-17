e5e828c65c104d3faeec1ab5de0040ee216dbdf0
dk2430098 <dk2430098@gmail.com>
2025-06-18 01:56:42 +0530

    Best Optimal sol

--- buy_and_sell_stock.cpp
+++ buy_and_sell_stock.cpp
@@ -7,6 +7,26 @@ class Solution
 public:
     int maxProfit(vector<int> &prices)
     {
+        int max_profit = 0;
+        int min_price = INT_MAX;
+
+        for (int price : prices)
+        {
+            if (price < min_price)
+                min_price = price;
+            else
+                max_profit = max(max_profit, price - min_price);
+        }
+
+        return max_profit;
+    }
+};
+
+class Solution1
+{
+public:
+    int maxProfit(vector<int> &prices)
+    {
         int profit = 0;
 
         for (int i = 0; i < prices.size() - 1; i++)
