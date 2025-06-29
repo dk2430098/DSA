dc03688fd35d83b5b3dcb32a846cd1d2b4231990
dk2430098 <dk2430098@gmail.com>
2025-06-30 05:03:07 +0530

    other

--- longest_substring_without_repeating_char.cpp
+++ longest_substring_without_repeating_char.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 #include <string>
+#include <unordered_map>
 using namespace std;
+
+class Solution // T.C = o(n) S.C = o(n)
+{
+public:
+    int lengthOfLongestSubstring(string s)
+    {
+        unordered_map<char, int> mp;
+        int max_len = 0, l = 0;
+
+        for (int r = 0; r < s.size(); r++)
+        {
+            char ch = s[r];
+            if (mp.find(ch) != mp.end())
+            {
+                l = max(l, mp[ch] + 1);
+            }
+            mp[ch] = r;
+            max_len = max(max_len, r - l + 1);
+        }
+
+        return max_len;
+    }
+};
 
 class Solution
 { // o(n3) o(n)
